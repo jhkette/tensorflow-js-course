fdaefa68afb135c34dcb97c279574c7824c562da
khenryjoe <joseph.ketterer@gmail.com>
2019-06-16 17:49:53 +0100

    use lodash to refactor

--- plinko/score.js
+++ plinko/score.js
@@ -11,31 +11,29 @@ function runAnalysis() {
   const testSetSize = 10
   const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
 
-  let numberCorrect = 0;
-  for (let i = 0; i <testSet.length; i++){
-   const bucket = knn(trainingSet, testSet[i][0]);
-   if(bucket === testSet[i][3]){
-     numberCorrect++;
-   }
-   
 
-  }
+  const accuracy = _.chain(testSet)
+  .filter(testPoint => 
+    knn(trainingSet, testPoint[0]) === testPoint[3]
+  )
+  .size()
+  .value()
+  console.log('accuracy' + accuracy);
 
-  console.log( 'Accuracy' + numberCorrect/testSetSize);
 }
 
-function knn(data, point){
- return  _.chain(data)
-  .map(row => [distance(row[0], point), row[3]])
-  .sortBy(row => row[0])
-  .slice(0, k)
-  .countBy(row => row[1])
-  .toPairs()
-  .sortBy(row => row[1])
-  .last()
-  .first()
-  .parseInt()
-  .value()
+function knn(data, point) {
+  return _.chain(data)
+    .map(row => [distance(row[0], point), row[3]])
+    .sortBy(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value()
 }
 
 
@@ -46,17 +44,9 @@ function distance(pointA, pointB) {
 }
 
 
-function splitDataset(data, testCount){
+function splitDataset(data, testCount) {
   const shuffled = _.shuffle(data);
-
   const testSet = _.slice(shuffled, 0, testCount);
   const trainingSet = _.slice(shuffled, testCount)
-
   return [testSet, trainingSet];
 }
-
-
-
-
-
-
